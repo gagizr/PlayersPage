f71d37575b8030106e3d34a02196c6ddf5ae902d
devgagi <65846480+devgagi@users.noreply.github.com>
2021-01-19 15:02:25 +0200

    Add project files.

---
+++ PlayersPage/Controllers/PlayersController.cs
@@ -0,0 +1,132 @@
+﻿
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PlayersPage.Models;
+
+namespace PlayersPage.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private PlayerDBContext db = new PlayerDBContext();
+
+        // GET: Players
+        public ActionResult Index()
+        {
+            return View(db.Players.ToList());
+        }
+
+        // GET: Players
+        public ActionResult List()
+        {
+            return PartialView(db.Players.ToList());
+        }
+
+        // GET: Players/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("Details", player);
+        }
+
+        // GET: Players/Create
+        public ActionResult Create()
+        {
+            return PartialView("Create");
+        }
+
+        // POST: Players/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Email")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Players.Add(player);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return PartialView("Create", player);
+        }
+
+        // GET: Players/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("Edit", player);
+        }
+
+        // POST: Players/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Email")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(player).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return PartialView("Edit", player);
+        }
+
+        // GET: Players/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("Delete", player);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Player player = db.Players.Find(id);
+            db.Players.Remove(player);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PlayersPage/Models/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlayersPage.Models
+{
+    public class Player
+    {
+
+        [Key]
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+        [Display(Name = "First Name:")]
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Minimum of 2 letters is required")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name:")]
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Minimum of 2 letters is required")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email:")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+    }
+
+    public class PlayerDBContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+    }
+
+}
